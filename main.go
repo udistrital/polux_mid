494c7bb2c1f215c9ee44c8ab314e8dc568fc74e9
JuanPiedrahita <juangonzalez1597@gmail.com>
2018-02-12 15:03:45 -0500

    Modificación del main para solucionar errores de cors

--- main.go
+++ main.go
@@ -6,22 +6,25 @@ import (
 	"github.com/astaxie/beego"
 )
 
+
 func main() {
-	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-	AllowOrigins: []string{"*"},
-	AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
-	AllowHeaders: []string{"Origin", "x-requested-with",
-	"content-type",
-	"accept",
-	"origin",
-	"authorization",
-	"x-csrftoken"},
-	ExposeHeaders: []string{"Content-Length"},
-	AllowCredentials: true,
-	}))
+	//orm.Debug = true
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders: []string{"Origin", "x-requested-with",
+			"content-type",
+			"accept",
+			"origin",
+			"authorization",
+			"x-csrftoken"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 	beego.Run()
 }
