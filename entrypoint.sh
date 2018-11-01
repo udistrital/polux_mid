d81de15011072683bc9ce366b3d0efe5ab331800
JuanPiedrahita <juangonzalez1597@gmail.com>
2018-11-01 12:19:48 -0500

    coments entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 set -u
 set -o pipefail
 
-if [ -n "${PARAMETER_STORE:-}" ]; then
+#if [ -n "${PARAMETER_STORE:-}" ]; then
   #export POLUX_MID__DB_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/polux_mid/db/username --output text --query Parameter.Value)"
   #export POLUX_MID__DB_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/polux_mid/db/password --output text --query Parameter.Value)"
-fi
+#fi
 
 exec ./main "$@"
