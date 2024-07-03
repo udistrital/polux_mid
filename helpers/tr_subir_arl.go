473a2f7e275a6ad823b782ae7a833ef88db05a1e
JDiegoHernandezA <jdiegohernandeza@gmail.com>
2024-07-03 13:28:07 -0500

    feat:Funcionalidad cargar ARL después de Rechazada udistrital/polux_cliente#585

--- helpers/tr_subir_arl.go
+++ helpers/tr_subir_arl.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
 	"github.com/udistrital/polux_mid/models"
 )
@@ -25,39 +26,97 @@ func AddTransaccionSubirArl(transaccion *models.TrSubirArl) (alerta []string, ou
 	var resDocumentoEscrito map[string]interface{}
 	if err := SendRequestNew("PoluxCrudUrl", url, "POST", &resDocumentoEscrito, &transaccion.DocumentoEscrito); err == nil { //Se guarda la ARL en Documento Escrito. Desde el Cliente ya viene con el tipo "Documentos adicionales asociados a las pasantias"
 
-		transaccion.DocumentoTrabajoGrado.Id = 0
-		transaccion.DocumentoEscrito.Id = int(resDocumentoEscrito["Id"].(float64))
-		transaccion.DocumentoTrabajoGrado.DocumentoEscrito.Id = int(resDocumentoEscrito["Id"].(float64))
+		var estadoRechazado []models.Parametro
+		url = "parametro?query=CodigoAbreviacion:ARC_PLX"
+		if err := GetRequestNew("UrlCrudParametros", url, &estadoRechazado); err != nil { //Se busca el Estado Trabajo de Grado "ARL Rechazada"
+			logs.Error(err.Error())
+			panic(err.Error())
+		}
 
-		url := "/v1/documento_trabajo_grado"
-		var resDocumentoTrabajoGrado map[string]interface{}
-		if err := SendRequestNew("PoluxCrudUrl", url, "POST", &resDocumentoTrabajoGrado, &transaccion.DocumentoTrabajoGrado); err == nil { //Se guarda la relación entre Documento Escrito y el Trabajo de Grado
+		if transaccion.TrabajoGrado.EstadoTrabajoGrado == estadoRechazado[0].Id { //Si el estado del trabajo de grado es "ARL Rechazado"
 
-			transaccion.DocumentoTrabajoGrado.Id = int(resDocumentoTrabajoGrado["Id"].(float64))
-
-			var parametro []models.Parametro
-			url = "parametro?query=CodigoAbreviacion:ACEA_PLX"
-			if err := GetRequestNew("UrlCrudParametros", url, &parametro); err != nil { //Se busca el Estado Trabajo de Grado "El estudiante cargó el certificado de afiliación de ARL y es necesaria la aprobación por parte de la Oficina de Extención de Pasantías"
+			var tipoDocumento []models.TipoDocumento
+			url = beego.AppConfig.String("UrlDocumentos") + "tipo_documento?query=CodigoAbreviacion:DPAS_PLX"
+			if err := GetJson(url, &tipoDocumento); err != nil { //Se busca el Tipo de Documento asociado a la ARL
 				logs.Error(err.Error())
 				panic(err.Error())
 			}
 
-			transaccion.TrabajoGrado.EstadoTrabajoGrado = parametro[0].Id
+			var documentosTG []models.DocumentoTrabajoGrado
+			url = beego.AppConfig.String("PoluxCrudUrl") + "/v1/documento_trabajo_grado?query=trabajo_grado__Id:" + strconv.Itoa(transaccion.TrabajoGrado.Id) + ",documento_escrito__tipo_documento_escrito:" + strconv.Itoa(tipoDocumento[0].Id)
+			if err := GetJson(url, &documentosTG); err != nil { //Se busca el registro en documento_trabajo_grado en la que relacione la ARL con el trabajo de grado
+				logs.Error(err.Error())
+				panic(err.Error())
+			}
 
-			url := "/v1/trabajo_grado/"+ strconv.Itoa(transaccion.TrabajoGrado.Id)
-			var resTrabajoGrado map[string]interface{}
-			if err := SendRequestNew("PoluxCrudUrl", url, "PUT", &resTrabajoGrado, &transaccion.TrabajoGrado); err != nil { //Se actualiza el Trabajo de Grado con el nuevo Estado
-				rollbackDocumentoTrabajoGradoARL(transaccion)
+			transaccion.DocumentoTrabajoGrado.Id = documentosTG[0].Id
+			transaccion.DocumentoEscrito.Id = int(resDocumentoEscrito["Id"].(float64))
+			transaccion.DocumentoTrabajoGrado.DocumentoEscrito.Id = int(resDocumentoEscrito["Id"].(float64))
+
+			url := "/v1/documento_trabajo_grado/"+ strconv.Itoa(documentosTG[0].Id)
+			var resDocumentoTrabajoGrado map[string]interface{}
+			if err := SendRequestNew("PoluxCrudUrl", url, "PUT", &resDocumentoTrabajoGrado, &transaccion.DocumentoTrabajoGrado); err == nil { //Se actualiza la relación entre la ARL y el Trabajo de Grado
+
+				transaccion.DocumentoTrabajoGrado.Id = int(resDocumentoTrabajoGrado["Id"].(float64))
+
+				var parametro []models.Parametro
+				url = "parametro?query=CodigoAbreviacion:ACEA_PLX"
+				if err := GetRequestNew("UrlCrudParametros", url, &parametro); err != nil { //Se busca el Estado Trabajo de Grado "ARL Cargada, en espera de aprobación"
+					logs.Error(err.Error())
+					panic(err.Error())
+				}
+
+				transaccion.TrabajoGrado.EstadoTrabajoGrado = parametro[0].Id
+
+				url := "/v1/trabajo_grado/" + strconv.Itoa(transaccion.TrabajoGrado.Id)
+				var resTrabajoGrado map[string]interface{}
+				if err := SendRequestNew("PoluxCrudUrl", url, "PUT", &resTrabajoGrado, &transaccion.TrabajoGrado); err != nil { //Se actualiza el Trabajo de Grado con el nuevo Estado
+					rollbackDocumentoTrabajoGradoARL(transaccion)
+					logs.Error(err)
+					panic(err.Error())
+				}
+
+			} else {
+				rollbackDocumentoEscritoARL(transaccion)
 				logs.Error(err)
 				panic(err.Error())
 			}
 
-		} else {
-			rollbackDocumentoEscritoARL(transaccion)
-			logs.Error(err)
-			panic(err.Error())
-		}
+		} else { //Si es la primera ARL que sube el estudiante
 
+			transaccion.DocumentoTrabajoGrado.Id = 0
+			transaccion.DocumentoEscrito.Id = int(resDocumentoEscrito["Id"].(float64))
+			transaccion.DocumentoTrabajoGrado.DocumentoEscrito.Id = int(resDocumentoEscrito["Id"].(float64))
+
+			url := "/v1/documento_trabajo_grado"
+			var resDocumentoTrabajoGrado map[string]interface{}
+			if err := SendRequestNew("PoluxCrudUrl", url, "POST", &resDocumentoTrabajoGrado, &transaccion.DocumentoTrabajoGrado); err == nil { //Se guarda la relación entre Documento Escrito y el Trabajo de Grado
+
+				transaccion.DocumentoTrabajoGrado.Id = int(resDocumentoTrabajoGrado["Id"].(float64))
+
+				var parametro []models.Parametro
+				url = "parametro?query=CodigoAbreviacion:ACEA_PLX"
+				if err := GetRequestNew("UrlCrudParametros", url, &parametro); err != nil { //Se busca el Estado Trabajo de Grado "ARL Cargada, en espera de aprobación"
+					logs.Error(err.Error())
+					panic(err.Error())
+				}
+
+				transaccion.TrabajoGrado.EstadoTrabajoGrado = parametro[0].Id
+
+				url := "/v1/trabajo_grado/" + strconv.Itoa(transaccion.TrabajoGrado.Id)
+				var resTrabajoGrado map[string]interface{}
+				if err := SendRequestNew("PoluxCrudUrl", url, "PUT", &resTrabajoGrado, &transaccion.TrabajoGrado); err != nil { //Se actualiza el Trabajo de Grado con el nuevo Estado
+					rollbackDocumentoTrabajoGradoARL(transaccion)
+					logs.Error(err)
+					panic(err.Error())
+				}
+
+			} else {
+				rollbackDocumentoEscritoARL(transaccion)
+				logs.Error(err)
+				panic(err.Error())
+			}
+		}
 	} else {
 		logs.Error(err)
 		panic(err.Error())
