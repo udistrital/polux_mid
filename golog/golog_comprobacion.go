4281776faa6b91aeaf6371288ac0934dc5b0c94e
JDiegoHernandezA <jdiegohernandeza@gmail.com>
2024-10-25 12:27:28 -0500

    fix: Correccion errores Sonarqube udistrital/polux_cliente#791

--- golog/golog_comprobacion.go
+++ golog/golog_comprobacion.go
@@ -7,11 +7,11 @@ import (
 )
 
 // Comprobar ...
-func Comprobar(reglas string, regla_inyectada string) (rest string) {
+func Comprobar(reglas string, reglaInyectada string) (rest string) {
 
 	//******QUITAR ARREGLO, DEJAR UNA SOLA VARIABLE PARA LAS REGLAS ******
 	m := golog.NewMachine().Consult(reglas)
-	if m.CanProve(regla_inyectada) {
+	if m.CanProve(reglaInyectada) {
 		rest = "true"
 	} else {
 		rest = "false"
@@ -22,12 +22,12 @@ func Comprobar(reglas string, regla_inyectada string) (rest string) {
 }
 
 // Obtener ...
-func Obtener(reglas string, regla_inyectada string) (rest string) {
+func Obtener(reglas string, reglaInyectada string) (rest string) {
 
 	var res string
 	m := golog.NewMachine().Consult(reglas)
 
-	resultados := m.ProveAll(regla_inyectada)
+	resultados := m.ProveAll(reglaInyectada)
 	for _, solution := range resultados {
 		res = fmt.Sprintf("%s", solution.ByName_("Y"))
 	}
