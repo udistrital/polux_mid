638327739cf4f880302d88c2b69570e7ed57e35d
JulianAvella <julianavella01@gmail.com>
2025-09-25 10:39:47 -0500

    feat: Ajuste de los cambios que estaban en la rama 'feat/pruebasUnitarias1' (udistrital/sga_documentacion#637)

--- golog/golog_comprobacion.go
+++ golog/golog_comprobacion.go
@@ -4,52 +4,18 @@ import (
 	"fmt"
 
 	golog "github.com/mndrix/golog"
-
 )
 
 // Comprobar ...
 func Comprobar(reglas string, reglaInyectada string) (rest string) {
 
-	fmt.Println("=== INICIO Comprobar ===")
-	fmt.Println("Reglas recibidas:")
-	fmt.Println(reglas)
-	fmt.Println("Regla inyectada:", reglaInyectada)
-
 	//******QUITAR ARREGLO, DEJAR UNA SOLA VARIABLE PARA LAS REGLAS ******
-	/*m := golog.NewMachine().Consult(reglas)
+	m := golog.NewMachine().Consult(reglas)
 	if m.CanProve(reglaInyectada) {
 		rest = "true"
 	} else {
 		rest = "false"
-	}*/
-
-	// Crear máquina con reglas
-    m := golog.NewMachine().Consult(reglas)
-
-    // Hechos que deberían cumplirse para validacion_requisitos
-    pruebas := []string{
-        "estado(20212015031, activo).",
-        "porcentaje_tg(80).",                  // depende de lo que cargues
-        "nivel_carrera(pregrado).",            // idem
-        "cursado(20212015031, 75).",
-        "nivel(20212015031, pregrado).",
-    }
-
-    for _, p := range pruebas {
-        ok := m.CanProve(p)
-        fmt.Printf("¿Se cumple %-40s ? -> %v\n", p, ok)
-    }
-
-    // Probar la regla completa
-    if m.CanProve(reglaInyectada) {
-        fmt.Println("✔ La regla se cumple:", reglaInyectada)
-        rest = "true"
-    } else {
-        fmt.Println("❌ La regla NO se cumple:", reglaInyectada)
-        rest = "false"
-    }
-
-	fmt.Println("=== FIN Comprobar ===")
+	}
 
 	return
 
