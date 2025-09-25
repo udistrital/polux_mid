5f12ed1ec40fbbdf5a78039cfb95ce7a375e2272
JulianAvella <julianavella01@gmail.com>
2025-09-25 09:55:33 -0500

    feat: Se suben los cambios que estaban en la rama 'feat/pruebasUnitarias1' (udistrital/sga_documentacion#637)

--- controllers/verificarRequisitos.go
+++ controllers/verificarRequisitos.go
@@ -42,6 +42,8 @@ func stringInSlice(str string, list []string) bool {
 // @router /CantidadModalidades [post]
 func (c *VerificarRequisitosController) CantidadModalidades() {
 
+	fmt.Println("verificaaa: ")
+
 	defer helpers.ErrorController(c.Controller, "VerificarRequisitosController")
 	reglasBase := ruler.CargarReglasBase("RequisitosModalidades")
 	if reglasBase != "" {
@@ -121,8 +123,10 @@ func (c *VerificarRequisitosController) Registrar() {
 				Modalidad 8: Creación o Interpretación (Estado, Porcentaje, Nivel)+(Tipo carrera)
 			*/
 
+			fmt.Println("DATOS RECIBIDOS EN EL BACKEND: ", v)
 			codigo := v.Codigo
 			modalidad := v.Modalidad
+			fmt.Println("Modalidad Modalidad Modalidad Modalidad: ", modalidad)
 			//estado in (J, A, ...)
 			//estado:=v.Estado
 			//Realiza la lectura del estado
@@ -150,7 +154,8 @@ func (c *VerificarRequisitosController) Registrar() {
 				reglasbase = reglasbase + "tipo_carrera(" + codigo + ", " + tipoCarrera + ")."
 				comprobacion = "validacion_creacion(" + codigo + ")."
 			}
-			fmt.Println(reglasbase)
+			fmt.Println("REGLAS BASE: ", reglasbase)
+			fmt.Println("comprobacion: ", comprobacion)
 
 			r := golog.Comprobar(reglasbase, comprobacion)
 
