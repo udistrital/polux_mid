0df5d118fdc64d829a590c91c61d19aebe98eec0
JulianAvella <julianavella01@gmail.com>
2026-02-02 15:25:09 -0500

    feat: Se remueve codigo innecesario (udistrital/polux_mid)

--- main.go
+++ main.go
@@ -33,8 +33,6 @@ func main() {
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 	}))
-	logs.Info("Iniciando aplicación polux_mid...")
-	logs.Info(beego.BConfig.RunMode)
 	beego.ErrorController(&customerrorv2.CustomErrorController{})
 	if err := xray.InitXRay(); err != nil {
 		logs.Error("error configurando AWS XRay: %v", err)
